7a10374324dc1dd106a49b97a3900416f00d88b1
rbeazer <54771210+rbeazer@users.noreply.github.com>
2020-11-03 10:23:42 +0000

    Lab 2 start
    Start of lab 2, task 1

--- main.c
+++ main.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World\n");
+    int a = 0;
+    int b = 0;
+    int c = 0;
+    int d = 0;
+    printf("Enter a number \n");
+    scanf("%d %d %d %d", &a,&b,&c,&d);
+    printf("You entered %d and %d and %d and %d", a, b, c, d);
     return 0;
 }
